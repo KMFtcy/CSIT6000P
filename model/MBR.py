dc8a757b2d2fa530b094d3313d9cdf0df5fa7344
kmftcy <kmftangchaoyang@gmail.com>
2022-04-26 05:33:23 +0000

    add check mbr is intersect

--- model/MBR.py
+++ model/MBR.py
@@ -1,3 +1,4 @@
+import MBR
 class MBR:
     def __init__(self, top=0, bottom=float('inf'), left=float('inf'), right=0):
         self.top = top
@@ -11,3 +12,11 @@ class MBR:
         result += "\tleft: " + str(self.left) + '\n'
         result += "\tright: " + str(self.right) + '\n}'
         return result
+
+    @staticmethod
+    def isIntersect(mbr1:MBR, mbr2:MBR):
+        newLeft = max(mbr1.left, mbr2.left)
+        newBottom = max(mbr1.bottom, mbr2.bottom)
+        newRight = min(mbr1.right, mbr2.right)
+        newTop = min(mbr1.top, mbr2.top)
+        return not ((newLeft > newRight) or (newBottom > newTop))
